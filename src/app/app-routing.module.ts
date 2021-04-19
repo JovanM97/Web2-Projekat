0e6de909dde1524caa2137d6b6a397dc41f05434
Jovan <godlike.jovan@gmail.com>
2021-04-19 20:55:29 +0200

    Login front done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginPageComponent},
+  {path: 'login', component: LoginPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
